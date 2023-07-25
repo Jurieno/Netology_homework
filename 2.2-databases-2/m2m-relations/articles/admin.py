3c262ef9eaba20acd3cf2a92f7a8a081f1ada735
Jurieno <94364464+Jurieno@users.noreply.github.com>
2023-07-26 03:39:04 +0700

    2.2

---
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -0,0 +1,31 @@
+from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
+
+from .models import Article, Tag
+
+class TagshipInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        is_main = 0
+        for form in self.forms:
+            if form.cleaned_data['is_main']:
+                is_main += 1
+
+                if is_main > 1:
+                    raise ValidationError('Основной тег должен быть один')
+        
+        if is_main == 0:
+            raise ValidationError('Выберите основной тег')
+        
+        return super().clean()  
+
+class TagshipInline(admin.TabularInline):
+    model = Tag.articles.through
+    formset = TagshipInlineFormset
+    extra = 0
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    inlines = [TagshipInline]
+
+
